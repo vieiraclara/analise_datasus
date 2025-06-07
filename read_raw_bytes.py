71f7305b5aa01e3047306a87dd99c837b8da2a24
Ana Clara <137239531+vieiraclara@users.noreply.github.com>
2025-06-07 19:02:08 -0300

    Delete read_raw_bytes.py

--- read_raw_bytes.py
+++
@@ -1,8 +0,0 @@
-with open("vigilancia_cianobacterias_cianotoxinas.csv", "rb") as f:
-    f.seek(0) # Go to the beginning of the file
-    for i, line_bytes in enumerate(f):
-        if i == 1471: # Line 1472 (index 1471)
-            print(f"Linha {i+1} (bytes): {line_bytes}")
-            break
-
-
